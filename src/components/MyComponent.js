97cd052c5dd21059dd22fbc2ead74e8ff2d57568
Le Truong Ky <kykhung123123@gmail.com>
2025-10-07 23:05:57 +0700

    Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,18 @@
+//class component
+//func component
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+
+            <div>My Component
+
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;

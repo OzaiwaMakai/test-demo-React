d3f0ae7eea820342d458cdf00ad41421c1153c6f
Le Truong Ky <kykhung123123@gmail.com>
2025-10-12 15:14:34 +0700

    props

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class DisplayInfor extends React.Component {
+    render() {
+        //destructuring array/object
+
+        console.log(this.props)
+        const { age, name } = this.props
+        return (
+            <div>
+                <div>My name is {name}</div>
+                <div>My age is {age}</div>
+            </div>
+        )
+    }
+}
+
+export default DisplayInfor
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,14 +2,21 @@
 //func component
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
     //JSX
 
     render() {
+        const myAge = [20, 30, 40]
+        const myInfo = ['a', 'bc']
         return (
             <div>
                 <UserInfor></UserInfor>
+                <br />
+                <br />
+                <DisplayInfor name="TKKkkkkkk" age="30"></DisplayInfor>
+                <DisplayInfor name="Truong Ky" age={26} myInfo={myInfo}></DisplayInfor>
             </div>
         );
     }
